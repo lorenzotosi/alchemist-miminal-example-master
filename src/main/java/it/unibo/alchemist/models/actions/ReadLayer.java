b76046a558de10c0abd14cf09f82c873754f6574
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-01-17 15:59:53 +0100

    Read molecule value

--- src/main/java/it/unibo/alchemist/models/actions/ReadLayer.java
+++ src/main/java/it/unibo/alchemist/models/actions/ReadLayer.java
@@ -12,60 +12,27 @@ public class ReadLayer<P extends Position<P> & Position2D<P>> extends AbstractAc
     private Molecule molecule = new SimpleMolecule("pheromone"); // molecule to be used to store the counter
     private final Node<Double> node; // implicit value
     private final Environment<Double, P> environment; // implicit value
-    private final int nodeCount;
 
-    /*public ReadLayer(Node<Integer> node, Environment<Integer, P> environment, Molecule layer) {
+    public ReadLayer(Node<Double> node, Environment<Double, P> environment) {
         super(node);
         this.node = node;
         this.environment = environment;
-        this.mValue = layer;
-
-    }
-
-        @Override
-    public Action<Integer> cloneAction(Node<Integer> node, Reaction<Integer> reaction) {
-        return new ReadLayer(node,  environment, mValue);
-    }
-
-    */
-
-    public ReadLayer(Node<Double> node, Environment<Double, P> environment, int nodeCount) {
-        super(node);
-        this.node = node;
-        this.environment = environment;
-        this.nodeCount = nodeCount;
     }
 
     @Override
     public Action<Double> cloneAction(Node<Double> node, Reaction<Double> reaction) {
-        return new ReadLayer<>(node,  environment, nodeCount);
+        return new ReadLayer<>(node,  environment);
     }
 
     @Override
     public void execute() {
-        //var layerValue = environment.getLayer(mValue).get().getValue(environment.getPosition(node));
-        //node.setConcentration(molecule, layerValue);
-
-
         Optional<Layer<Double, P>> OptPhL = environment.getLayer(molecule);
         if (OptPhL.isPresent()){
             PheromoneLayer<P> layer = (PheromoneLayer<P>) OptPhL.get();
             var pos = environment.getPosition(node);
-            layer.addToMap(pos, node.getConcentration(molecule));
+            //layer.addToMap(pos, node.getConcentration(molecule));
+            node.setConcentration(molecule, layer.getValue(pos));
         }
-
-        /*if (environment.getNodeCount() < nodeCount){
-            var prototype = environment.getNodes().iterator().next();
-            var xx = environment.getPosition(prototype);
-            var node = prototype.cloneNode(environment.getSimulation().getTime());
-            environment.addNode(node, environment
-                    .getPosition(prototype)
-                    .plus(environment.makePosition(0, -1).getCoordinates()));
-        }*/
-
-        /*var x = environment.getLayer(molecule).get().getValue(environment.getPosition(node));
-        System.out.println(environment.getPosition(node).toString());
-        System.out.println(x);*/
 
     }
 
