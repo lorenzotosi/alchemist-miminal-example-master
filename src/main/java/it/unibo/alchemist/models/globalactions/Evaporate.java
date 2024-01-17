5623598666ef545d1469bac3a4ddf7df21ae536a
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-01-17 17:03:32 +0100

    improved evaporate method

--- src/main/java/it/unibo/alchemist/models/globalactions/Evaporate.java
+++ src/main/java/it/unibo/alchemist/models/globalactions/Evaporate.java
@@ -4,16 +4,21 @@ import it.unibo.alchemist.model.*;
 import it.unibo.alchemist.model.molecules.SimpleMolecule;
 import it.unibo.alchemist.models.layers.PheromoneLayer;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Evaporate <P extends Position<P> & Position2D<P>> extends AbstractGlobalReaction<Double, P> {
     public Evaporate(Environment<Double, P> environment, TimeDistribution<Double> distribution) {
         super(environment, distribution);
     }
 
+    /**
+     * get environment -> get layer -> get map -> foreach key -> update value or remove if value is <=0
+     */
     @Override
-    protected void action() {
+    /*protected void action() {
         Environment<Double, P> e = this.getEnvironment();
         Optional<Layer<Double, P>> OptPhL = e.getLayer(new SimpleMolecule("pheromone"));
         if (OptPhL.isPresent()){
@@ -23,10 +28,30 @@ public class Evaporate <P extends Position<P> & Position2D<P>> extends AbstractG
                 for (var entry : pheromoneMap.entrySet()) {
                     var key = entry.getKey();
                     Double value = entry.getValue() * 0.9;
-                    pheromoneMap.put(key, value);
+                    if (value <= 0.0){
+                        pheromoneMap.remove(key, value);
+                    } else {
+                        pheromoneMap.put(key, value);
+                    }
                 }
             }
         }
+    }*/
+    protected void action() {
+        Environment<Double, P> environment = this.getEnvironment();
+        Optional<Layer<Double, P>> optionalPheromoneLayer = environment.getLayer(new SimpleMolecule("pheromone"));
+
+        if (optionalPheromoneLayer.isPresent()) {
+            PheromoneLayer<P> layer = (PheromoneLayer<P>) optionalPheromoneLayer.get();
+            var pheromoneMap = layer.getMap();
+
+            pheromoneMap.entrySet().removeIf(entry -> {
+                double updatedValue = entry.getValue() * 0.9;
+                pheromoneMap.put(entry.getKey(), updatedValue);
+                return updatedValue <= 0.0;
+            });
+        }
     }
+
 
 }
