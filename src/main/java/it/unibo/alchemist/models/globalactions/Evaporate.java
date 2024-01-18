be04cf916a4c62dcf99504cfbdf3b93cc0d9f8c8
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-01-18 11:23:19 +0100

    rename map

--- src/main/java/it/unibo/alchemist/models/globalactions/Evaporate.java
+++ src/main/java/it/unibo/alchemist/models/globalactions/Evaporate.java
@@ -4,10 +4,7 @@ import it.unibo.alchemist.model.*;
 import it.unibo.alchemist.model.molecules.SimpleMolecule;
 import it.unibo.alchemist.models.layers.PheromoneLayer;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class Evaporate <P extends Position<P> & Position2D<P>> extends AbstractGlobalReaction<Double, P> {
     public Evaporate(Environment<Double, P> environment, TimeDistribution<Double> distribution) {
@@ -20,9 +17,9 @@ public class Evaporate <P extends Position<P> & Position2D<P>> extends AbstractG
     @Override
     /*protected void action() {
         Environment<Double, P> e = this.getEnvironment();
-        Optional<Layer<Double, P>> OptPhL = e.getLayer(new SimpleMolecule("pheromone"));
+        Optional<Layer<Double, P>> optionalPheromoneLayer = e.getLayer(new SimpleMolecule("pheromone"));
         if (OptPhL.isPresent()){
-            PheromoneLayer<P> layer = (PheromoneLayer<P>) OptPhL.get();
+            PheromoneLayer<P> layer = (PheromoneLayer<P>) optionalPheromoneLayer.get();
             var pheromoneMap = layer.getMap();
             if(!pheromoneMap.isEmpty()) {
                 for (var entry : pheromoneMap.entrySet()) {
