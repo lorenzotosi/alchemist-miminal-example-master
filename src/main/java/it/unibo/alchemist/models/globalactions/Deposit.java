cd569d0a73b5456caebe7673a62f83f841323407
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-01-30 17:22:14 +0100

    deposit always the same value

--- src/main/java/it/unibo/alchemist/models/globalactions/Deposit.java
+++ src/main/java/it/unibo/alchemist/models/globalactions/Deposit.java
@@ -6,11 +6,13 @@ import org.danilopianini.util.ListSet;
 
 public class Deposit<P extends Position<P> & Position2D<P>> extends AbstractGlobalReaction<Double, P> {
     private final ListSet<Node<Double>> node;
+    private final Double pheromoneDepositValue;
 
     public Deposit(final TimeDistribution<Double> distribution, final Environment<Double, P> environment,
-                   final Molecule molecule) {
+                   final Molecule molecule, final Double pheromoneDepositValue) {
         super(environment, distribution, molecule);
         this.node = environment.getNodes();
+        this.pheromoneDepositValue = pheromoneDepositValue;
     }
 
     @Override
@@ -19,7 +21,7 @@ public class Deposit<P extends Position<P> & Position2D<P>> extends AbstractGlob
 
         for (var node : this.node) {
             var pos = environment.getPosition(node);
-            phLayer.addToMap(pos, node.getConcentration(this.getMolecule()));
+            phLayer.addToMap(pos, pheromoneDepositValue);
         }
     }
 
