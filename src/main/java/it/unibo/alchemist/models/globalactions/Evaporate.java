590e5733f8cf9bddad54fc318c73f8af23563d2b
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-01-31 19:17:30 +0100

    Update Evaporate.java

--- src/main/java/it/unibo/alchemist/models/globalactions/Evaporate.java
+++ src/main/java/it/unibo/alchemist/models/globalactions/Evaporate.java
@@ -1,7 +1,10 @@
 package it.unibo.alchemist.models.globalactions;
 
 import it.unibo.alchemist.model.*;
+import it.unibo.alchemist.models.layers.Patch;
 import it.unibo.alchemist.models.layers.PheromoneLayer;
+
+import java.util.Arrays;
 
 public class Evaporate<P extends Position<P> & Position2D<P>> extends AbstractGlobalReaction<Double, P> {
     public Evaporate(final Environment<Double, P> environment, final TimeDistribution<Double> distribution,
@@ -9,34 +12,16 @@ public class Evaporate<P extends Position<P> & Position2D<P>> extends AbstractGl
         super(environment, distribution, molecule);
     }
 
-    /**
-     * get environment -> get layer -> get map -> foreach key -> update value or remove if value is <=0
-     */
     @Override
-    /*protected void action() {
-        Environment<Double, P> environment = this.getEnvironment();
-        Optional<Layer<Double, P>> optionalPheromoneLayer = environment.getLayer(this.getMolecule());
-
-        if (optionalPheromoneLayer.isPresent()) {
-            PheromoneLayer<P> layer = (PheromoneLayer<P>) optionalPheromoneLayer.get();
-            var pheromoneMap = layer.getMap();
-
-            pheromoneMap.entrySet().removeIf(entry -> {
-                double updatedValue = entry.getValue() * 0.9;
-                pheromoneMap.put(entry.getKey(), updatedValue);
-                return updatedValue <= 0.0;
-            });
-        }
-*/
     protected void action(final PheromoneLayer<P> phLayer) {
-        var pheromoneMap = phLayer.getMap();
-        pheromoneMap.entrySet().removeIf(entry -> {
-            double updatedValue = entry.getValue() * 0.9;
-            if (updatedValue > 0.0){
-                pheromoneMap.put(entry.getKey(), updatedValue);
+        var pheromoneMap = phLayer.getPatches();
+
+        for (Patch[] patches : pheromoneMap) {
+            for (Patch patch : patches) {
+                double updatedValue = patch.getPheromoneConcentration() * 0.9;
+                patch.setPheromoneConcentration(updatedValue);
             }
-            return updatedValue <= 0.0;
-        });
+        }
     }
 
 
