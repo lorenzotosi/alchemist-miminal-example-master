517f6c47f23eb1b1637905cd93651918a921452e
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-01-31 18:45:20 +0100

    single patch class

---
+++ src/main/java/it/unibo/alchemist/models/layers/Patch.java
@@ -0,0 +1,25 @@
+package it.unibo.alchemist.models.layers;
+
+import java.awt.*;
+import java.util.Arrays;
+
+public class Patch  {
+
+    private double pheromoneConcentration;
+    private final int x;
+    private final int y;
+
+    public Patch(int x, int y, double pheromoneConcentration) {
+        this.pheromoneConcentration = pheromoneConcentration;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Double getPheromoneConcentration(){
+        return this.pheromoneConcentration;
+    }
+
+    public void setPheromoneConcentration(final double value){
+        this.pheromoneConcentration = value;
+    }
+}
