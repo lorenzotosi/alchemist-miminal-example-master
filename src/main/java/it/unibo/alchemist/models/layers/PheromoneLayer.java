17f8bcec1702af77fc91c85b36b8827c1c38dacd
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-02-02 16:10:33 +0100

    use map instead of array

--- src/main/java/it/unibo/alchemist/models/layers/PheromoneLayer.java
+++ src/main/java/it/unibo/alchemist/models/layers/PheromoneLayer.java
@@ -6,14 +6,12 @@ import it.unibo.alchemist.model.Molecule;
 import it.unibo.alchemist.model.Position2D;
 import kotlin.Pair;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class PheromoneLayer<P extends Position2D<P>> implements Layer<Double, P> {
 
     private final Environment<Double, P> environment;
-    private final Patch[][] patches;
+    private final Map<Patch, Double> patches;
 
     private final Molecule molecule;
 
@@ -21,26 +19,27 @@ public class PheromoneLayer<P extends Position2D<P>> implements Layer<Double, P>
                           final int width, final int height) {
         this.environment = environment;
         this.molecule = molecule;
-        this.patches = new Patch[width][height];
+        this.patches = new LinkedHashMap<>();
         setupPatches(width, height);
 
     }
 
-    private void setupPatches(final int width, final int height) {
-        for(int x = 0; x < width; x++)
-            for(int y = 0; y < height; y++)
-                this.patches[x][y] = new Patch(x, y, 0.0);
+    private void setupPatches(final int width, final int height, final Double step,
+                              final Double startX, final Double startY) {
+        for(Double x = startX; x < width; x = x + step)
+            for(Double y = startY; y < height; y = y + step)
+                this.patches.put(new Patch(x, y), 0.0);
     }
 
     @Override
     public Double getValue(final P p) {
-        return patches[(int)p.getX()][(int)p.getY()].getPheromoneConcentration();
+        return patches.getOrDefault(new Patch(p.getX(), p.getY()), 0.0);
     }
 
     public void addPheromone(final P p, final double concentration){
-        patches[(int)p.getX()][(int)p.getY()].increasePheromoneConcentration(concentration);
+
     }
-    public Patch[][] getPatches() {
+    public Map<Patch, Double> getPatches() {
         return patches;
     }
 
