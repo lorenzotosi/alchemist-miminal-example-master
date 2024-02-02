8d38c819057cd755145bfd19ae119b9bc1194eeb
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-02-02 20:33:11 +0100

    manage position in new way

--- src/main/java/it/unibo/alchemist/models/layers/PheromoneLayer.java
+++ src/main/java/it/unibo/alchemist/models/layers/PheromoneLayer.java
@@ -15,21 +15,44 @@ public class PheromoneLayer<P extends Position2D<P>> implements Layer<Double, P>
     private final Environment<Double, P> environment;
 
     //p posizione, double valore
-    private Map<P, Double> map;
+    private final Map<P, Double> map;
+    private final Double step;
+    private final Double width;
+    private final Double height;
 
     private final Molecule molecule;
 
-    public PheromoneLayer(Environment<Double, P> environment, Molecule molecule) {
+    public PheromoneLayer(final Environment<Double, P> environment, final Molecule molecule,
+                          final Double startX, final Double startY,
+                          final Double width, final Double height, final Double step) {
         this.environment = environment;
         this.molecule = molecule;
         this.map = new HashMap<>();
+        this.step = step;
+        this.width = width;
+        this.height = height;
+
+        for (double x = startX; x < width - Math.abs(startX); x = x + step){
+            for (double y = startY; y < height - Math.abs(startY); y = y + step){
+                map.put(environment.makePosition(x, y), 0.0);
+            }
+        }
+
+        var x1 = roundToClosestPosition(-7.23, 0.25, 10);
+        var x2 = roundToClosestPosition(7.23, 0.5, 10);
+        var x3 = roundToClosestPosition(7.23, 1, 10);
+        var x4 = roundToClosestPosition(5.73, 0.25, 10);
+        var x5 = roundToClosestPosition(0.23, 0.25, 10);
+
+        System.out.println("a");
     }
 
     public void addToMap(P p, Double value){
-        if(map.containsKey(p))
-            map.put(p, (value + map.get(p)));
-        else
-            map.put(p, value);
+        var mapPosition = adaptPosition(p);
+        if(map.containsKey(mapPosition))
+            map.put(mapPosition, (value + map.get(mapPosition)));
+        //else
+            //map.put(mapPosition, value);
     }
 
     public Map<P, Double> getMap() {
@@ -39,7 +62,29 @@ public class PheromoneLayer<P extends Position2D<P>> implements Layer<Double, P>
 
     @Override
     public Double getValue(P p) {
-        return map.getOrDefault(p, 0.0);
+        return map.getOrDefault(adaptPosition(p), 0.0);
+    }
+
+    private double roundToClosestPosition(final double value, final double step, double maxValue) {
+        int nSteps = (int) (value / step);
+        double roundValue = nSteps * step;
+
+        if (roundValue > maxValue) {
+            return maxValue;
+        }
+
+        double nextRoundValue = (nSteps + 1) * step;
+        if (Math.abs(value - roundValue) <= Math.abs(value - nextRoundValue)) {
+            return roundValue;
+        } else {
+            return nextRoundValue;
+        }
+    }
+
+    public P adaptPosition(final P position){
+        var x = roundToClosestPosition(position.getX(), step, width);
+        var y = roundToClosestPosition(position.getY(), step, height);
+        return environment.makePosition(x, y);
     }
 
 }
