bc292303a1381e9055af91ae738f5fe75365e963
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-02-08 16:27:36 +0100

    add doc

--- src/main/java/it/unibo/alchemist/models/layers/PheromoneLayer.java
+++ src/main/java/it/unibo/alchemist/models/layers/PheromoneLayer.java
@@ -3,9 +3,16 @@ package it.unibo.alchemist.models.layers;
 import it.unibo.alchemist.model.Layer;
 import it.unibo.alchemist.model.Position2D;
 
+
+/**
+ * Represents a pheromone layer in a grid-based model.
+ * The layer allows depositing pheromone values at specific positions.
+ *
+ * @param <P> The type of position.
+ */
 public interface PheromoneLayer<P extends Position2D<P>> extends Layer<Double, P> {
     /**
-     * Adds to the map in the p position the value
+     * Adds the specified pheromone value to the map at the given position.
      *
      * @param p The patch position
      * @param value The pheromone value associated to the grid position
