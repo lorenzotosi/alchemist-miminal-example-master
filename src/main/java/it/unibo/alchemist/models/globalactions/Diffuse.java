16fa139460917cc2245af34f5a46d32dccb906f7
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-02-08 16:27:47 +0100

    add doc

--- src/main/java/it/unibo/alchemist/models/globalactions/Diffuse.java
+++ src/main/java/it/unibo/alchemist/models/globalactions/Diffuse.java
@@ -9,11 +9,27 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 
+/**
+ * This class represents a global reaction that diffuses the pheromone value to it's neighborhood.
+ *
+ * @param <T> The type of the molecule being diffused.
+ * @param <P> The type of the position in the environment.
+ */
 public class Diffuse<T, P extends Position<P> & Position2D<P>> extends AbstractGlobalReaction<T, P> {
     private final Double customDiffusionThreshold;
     private final Double neighborhoodDistance;
     private final Double diffusionValue;
 
+    /**
+     * Constructs a new Diffuse object.
+     *
+     * @param environment             The environment in which the diffusion takes place.
+     * @param distribution            The distribution used to determine the amount of molecule to diffuse.
+     * @param molecule                The molecule to be diffused.
+     * @param customDiffusionThreshold The custom diffusion threshold.
+     * @param neighborhoodDistance    The distance within which the diffusion occurs.
+     * @param diffusionValue          The value by which the molecule is diffused.
+     */
     public Diffuse(final Environment<T, P> environment, final TimeDistribution<T> distribution, final Molecule molecule,
                    final Double customDiffusionThreshold, final Double neighborhoodDistance,
                    final Double diffusionValue) {
@@ -23,6 +39,11 @@ public class Diffuse<T, P extends Position<P> & Position2D<P>> extends AbstractG
         this.diffusionValue = diffusionValue;
     }
 
+    /**
+     * Performs the diffusion action on the given pheromone layer.
+     *
+     * @param phLayer The pheromone layer on which the diffusion action is performed.
+     */
     @Override
     public void action(final PheromoneLayerImpl<P> phLayer) {
         var pheromoneMap = phLayer.getMap();
@@ -32,29 +53,18 @@ public class Diffuse<T, P extends Position<P> & Position2D<P>> extends AbstractG
             var nPos = phLayer.adaptPosition(p);
             var pValue = pheromoneMap.getOrDefault(nPos, 0.0);
             if (pValue > customDiffusionThreshold)
-                getNeighborhood(nPos).forEach(x -> dummyMap.put(x, pValue* diffusionValue));
+                getNeighborhood(nPos).forEach(x -> dummyMap.put(x, pValue * diffusionValue));
         });
         dummyMap.forEach(phLayer::deposit);
-
-        /*pheromoneMap.forEach((key, value) -> {
-                    if(value > customDiffusionThreshold){
-                        getNeighborhood(key).forEach(x -> dummyMap.put(x, value*diffusionValue));
-                    }
-                }
-        );
-        dummyMap.forEach(phLayer::addToMap);
-        dummyMap.clear();*/
-
     }
 
     /**
-     * ho una posizione, trovo il suo intorno e da queste nuove posizioni levo
-     * quelle che eventualmente sono quelle di un nodo
-     * @param position
-     * @return
+     * Retrieves the neighborhood positions around the given position.
+     *
+     * @param position The position for which the neighborhood positions are retrieved.
+     * @return A list of neighborhood positions.
      */
     private List<P> getNeighborhood(final P position) {
-        //var nodes = getEnvironment().getNodes().stream().map(a -> getEnvironment().getPosition(a)).toList();
         final var x = position.getX();
         final var y = position.getY();
         final double[] xs = DoubleStream.of(x - neighborhoodDistance, x, x + neighborhoodDistance).toArray();
@@ -62,7 +72,6 @@ public class Diffuse<T, P extends Position<P> & Position2D<P>> extends AbstractG
         return Arrays.stream(xs).boxed()
                 .flatMap(x1 -> Arrays.stream(ys).boxed().map(y1 -> getEnvironment().makePosition(x1, y1)))
                 .filter(p -> !p.equals(position))
-                //.filter(p -> !nodes.contains(p))
                 .collect(Collectors.toList());
     }
 }
--- src/main/java/it/unibo/alchemist/models/globalactions/Evaporate.java
+++ src/main/java/it/unibo/alchemist/models/globalactions/Evaporate.java
@@ -3,31 +3,43 @@ package it.unibo.alchemist.models.globalactions;
 import it.unibo.alchemist.model.*;
 import it.unibo.alchemist.models.layers.PheromoneLayerImpl;
 
+
+/**
+ * This class represents a global reaction that evaporates pheromones in a pheromone layer.
+ *
+ * @param <T> the type of the entities in the environment
+ * @param <P> the type of the positions in the environment
+ */
 public class Evaporate<T, P extends Position<P> & Position2D<P>> extends AbstractGlobalReaction<T, P> {
     private final Double evaporationValue;
+
+    /**
+     * Constructs a new Evaporate object with the specified environment, time distribution,
+     * molecule, and evaporation value.
+     *
+     * @param environment      the environment in which the reaction occurs
+     * @param distribution     the time distribution for the reaction
+     * @param molecule         the molecule associated with the reaction
+     * @param evaporationValue the value by which the pheromones will be evaporated
+     */
     public Evaporate(final Environment<T, P> environment, final TimeDistribution<T> distribution,
                      final Molecule molecule, final Double evaporationValue) {
         super(environment, distribution, molecule);
         this.evaporationValue = evaporationValue;
     }
 
+    /**
+     * Performs the action of evaporating pheromones in the pheromone layer.
+     *
+     * @param phLayer the pheromone layer on which the action is performed
+     */
     @Override
     public void action(final PheromoneLayerImpl<P> phLayer) {
         var pheromoneMap = phLayer.getMap();
-        /*pheromoneMap.entrySet().removeIf(entry -> {
-            double updatedValue = entry.getValue() * 0.9;
-            if (updatedValue > 0.0){
-                pheromoneMap.put(entry.getKey(), updatedValue);
-            }
-            return updatedValue <= 0.0;
-        });
-         */
         pheromoneMap.forEach((key, value) -> {
-            if(value > 0){
-                pheromoneMap.put(key, value*evaporationValue);
+            if (value > 0) {
+                pheromoneMap.put(key, value * evaporationValue);
             }
         });
     }
-
-
 }
