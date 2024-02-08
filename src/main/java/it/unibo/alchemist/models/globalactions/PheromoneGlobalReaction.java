db12c17f833514190dff861a61f87fbbcc635c68
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2024-02-08 16:28:46 +0100

    add doc

--- src/main/java/it/unibo/alchemist/models/globalactions/PheromoneGlobalReaction.java
+++ src/main/java/it/unibo/alchemist/models/globalactions/PheromoneGlobalReaction.java
@@ -5,7 +5,17 @@ import it.unibo.alchemist.model.Position;
 import it.unibo.alchemist.model.Position2D;
 import it.unibo.alchemist.models.layers.PheromoneLayerImpl;
 
+/**
+ * Represents a global reaction that acts on a PheromoneLayerImpl.
+ *
+ * @param <T> the type of data stored in the pheromone layer
+ * @param <P> the type of position used in the pheromone layer
+ */
 public interface PheromoneGlobalReaction<T, P extends Position<P> & Position2D<P>> extends GlobalReaction<T> {
+    /**
+     * Performs an action on the given PheromoneLayerImpl.
+     *
+     * @param phLayer the pheromone layer on which the action is performed
+     */
     void action(PheromoneLayerImpl<P> phLayer);
-
 }
